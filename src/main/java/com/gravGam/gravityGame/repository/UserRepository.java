dc628bdc854f08a81f2a3901871401af2f83ec0a
Bálint Áts <atsbalint@gmail.com>
2020-08-18 22:29:08 +0200

    runs, but there is an errormessage: Caused by: org.postgresql.util.PSQLException: ERROR: syntax error at or near -user-

--- src/main/java/com/gravGam/gravityGame/repository/UserRepository.java
+++ src/main/java/com/gravGam/gravityGame/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.gravGam.gravityGame.repository;
+import com.gravGam.gravityGame.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
