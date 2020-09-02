83d72214813702b4a66a595c0a3bfa1cbb2a599e
Bálint Áts <atsbalint@gmail.com>
2020-09-02 17:35:37 +0200

    Add fakeUsers in users list

--- src/main/java/com/gravGam/gravityGame/controller/UserController.java
+++ src/main/java/com/gravGam/gravityGame/controller/UserController.java
@@ -8,29 +8,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+
 public class UserController {
 
     @Autowired
     private UserStorage userStorage;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("ladder")
     public List<User> getUserLadder(){
         return this.userStorage.getUserLadder();
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/reg")
     public User register(@RequestBody User newUser){
         this.userStorage.addUser(newUser);
         return newUser;
     }
 
-    /**@PostMapping("/{userName}")
+    /**@CrossOrigin(origins = "*")
+     * @PostMapping("/{userName}")
         public User login(@PathVariable("userName") String userName) throws Exception {
         return UserStorage.findUser(userName);
     }**/
 
-    @PostMapping("/{userName}")
-    public String login(@PathVariable("userName") String userName){
-        return userName;
+/*    @CrossOrigin(origins = "*")
+    @PostMapping("/login")
+    public String login(@PathVariable("username") String username){
+        return username;
     }
+    return userStorage.findUser(username);*/
 }
--- src/main/java/com/gravGam/gravityGame/service/UserStorage.java
+++ src/main/java/com/gravGam/gravityGame/service/UserStorage.java
@@ -4,12 +4,18 @@ import com.gravGam.gravityGame.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserStorage {
 
-    private List<User> users = new ArrayList<User>();
+
+
+    private List<User> users = new ArrayList<User>(Arrays.asList( new User(5000000000L,"BackendFakeUser 1","dsfkhsf",3),
+            new User(5000000000L,"BackendFakeUser 2","dsfkhsf",2),
+            new User(5000000000L,"BackendFakeUser 3","dsfkhsf",8),
+            new User(5000000000L,"BackendFakeUser 4","dsfkhsf",3)));
 
     public User findUser(String userName) throws Exception {
         User foundUser = this.users.stream()
