d94f0bb546eeeae6ee5b7a76ad54f058f359c9d4
Bálint Áts <atsbalint@gmail.com>
2020-09-08 19:34:23 +0200

    Complete JwtUtil class

--- src/main/java/com/gravGam/gravityGame/util/JwtUtil.java
+++ src/main/java/com/gravGam/gravityGame/util/JwtUtil.java
@@ -1,8 +1,13 @@
 package com.gravGam.gravityGame.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtUtil {
@@ -22,10 +27,33 @@ public class JwtUtil {
         return claimResolver.apply(claims);
     }
 
-
-/*        return Jwts.parser()
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
                 .setSigningKey(SECRET_KEY)
                 .parseClaimsJws(token)
                 .getBody();
-    }*/
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token)
+                .before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> Claims = new HashMap<>();
+        return createToken(Claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String ,Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.ES256, SECRET_KEY)
+                .compact();
+    }
+
+
+
 }
